d266b12cd77b75bc96f38c87b0f508e6b00b5d83
Friendbird Alex <2565298515@qq.com>
2025-12-27 20:18:00 +0800

    判断二叉树是否对称（递归）

---
+++ .idea/Day22/isSymmetric.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return check(root.left, root.right);
+    }
+
+    private boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        boolean outside = check(p.left, q.right);
+        boolean inside = check(p.right, q.left);
+        return outside && inside;
+    }
+}
